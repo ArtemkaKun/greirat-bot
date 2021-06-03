758f66d3f0ecdfa45bfa8c93417b81fa342752c8
Artem Yurchenko <zumikq78@gmail.com>
2021-06-03 15:31:31 +0200

    Initial bot set up code was implemented

--- greirat/Program.cs
+++ greirat/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace greirat
 {
-    internal static class Program
+    internal class Program
     {
+        private const string ENVIRONMENT_VARIABLE_WITH_BOT_TOKEN = "FOOD_BOT_TOKEN";
+        
+        private DiscordSocketClient Client { get; set; } = new ();
+        
         private static void Main ()
         {
-            Console.WriteLine("Hello World!");
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        private async Task MainAsync()
+        {
+            SubscribeOnClientEvents();
+            await StartBot();
+            await Task.Delay(-1);
+        }
+
+        private void SubscribeOnClientEvents ()
+        {
+            Client.Log += Log;
+        }
+
+        private async Task StartBot ()
+        {
+            await Client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable(ENVIRONMENT_VARIABLE_WITH_BOT_TOKEN));
+            await Client.StartAsync();
+        }
+        
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            
+            return Task.CompletedTask;
         }
     }
 }

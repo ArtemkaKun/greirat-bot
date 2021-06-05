139520e52f223c2262c30a120b80a6c052789459
Artem Yurchenko <zumikq78@gmail.com>
2021-06-05 18:14:55 +0200

    Beautiful table output for Show commands was implemented

--- greirat/CommandsModule.cs
+++ greirat/CommandsModule.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
+using greirat.Helpers;
 
 namespace greirat
 {
     public class CommandsModule : ModuleBase<SocketCommandContext>
     {
-        private const string ORDER_WAS_SAVED_MESSAGE = "Order was proceeded";
+        private const string ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE = "Order was successfully ";
+        private const string ORDER_WAS_SAVED_MESSAGE = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "proceeded";
+        private const string ORDER_WAS_UPDATED_MESSAGE = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "updated";
+        private const string ORDER_WAS_REMOVED = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "removed";
+        private const string ORDER_UPDATE_FAILED = "Failed to update order";
+        private const string ORDER_DELETE_FAILED = "Failed to remove order";
+        private const string NOTHING_TO_SHOW_MESSAGE = "Nothing to show";
+
+        private OrderDataAsciiTableConverter AsciiTableConverter { get; set; } = new ();
 
         [Command("makeOrder")]
         [Summary("Creates new order")]
         public Task CreateNewOrder ([Remainder] string orderText)
         {
             DB.Instance.AddNewOrder(Context.Message.Author.Username, orderText);
+            
             return ReplyAsync(ORDER_WAS_SAVED_MESSAGE);
         }
         
@@ -23,7 +32,7 @@ namespace greirat
         {
             Queue<OrderData> todayOrders = DB.Instance.GetTodayOrders();
             
-            return ReplyAsync(FormOrdersShowData(todayOrders).ToString());
+            return ReplyAsync(todayOrders.Count == 0 ? NOTHING_TO_SHOW_MESSAGE : AsciiTableConverter.FormOrdersShowData(todayOrders).ToString());
         }
 
         [Command("showMyTodayOrders")]
@@ -32,7 +41,7 @@ namespace greirat
         {
             Queue<OrderData> todayOrders = DB.Instance.GetTodayOrders(Context.Message.Author.Username);
             
-            return ReplyAsync(FormOrdersShowData(todayOrders).ToString());
+            return ReplyAsync(todayOrders.Count == 0 ? NOTHING_TO_SHOW_MESSAGE : AsciiTableConverter.FormOrdersShowData(todayOrders).ToString());
         }
 
         [Command("updateOrder")]
@@ -41,7 +50,7 @@ namespace greirat
         {
             bool updateOperationResult = DB.Instance.TryUpdateOrderData(Context.Message.Author.Username, idOfOrder, newOrderMessage);
             
-            return ReplyAsync(updateOperationResult == true ? "Order was successfully updated" : "Failed to update");
+            return ReplyAsync(updateOperationResult == true ? ORDER_WAS_UPDATED_MESSAGE : ORDER_UPDATE_FAILED);
         }
 
         [Command("deleteOrder")]
@@ -50,20 +59,7 @@ namespace greirat
         {
             bool deleteOperationResult = DB.Instance.TryDeleteOrderData(Context.Message.Author.Username, idOfOrder);
             
-            return ReplyAsync(deleteOperationResult == true ? "Order was successfully removed" : "Failed to remove");
-        }
-
-        private StringBuilder FormOrdersShowData (Queue<OrderData> todayOrders)
-        {
-            StringBuilder todayOrdersTableBuilder = new();
-
-            while (todayOrders.Count > 0)
-            {
-                OrderData order = todayOrders.Dequeue();
-                todayOrdersTableBuilder.Append($"{order.OrderID.ToString()} {order.PersonName} {order.OrderText} \n");
-            }
-
-            return todayOrdersTableBuilder;
+            return ReplyAsync(deleteOperationResult == true ? ORDER_WAS_REMOVED : ORDER_DELETE_FAILED);
         }
     }
 }
---
+++ greirat/Helpers/OrderDataAsciiTableConverter.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using greirat.External;
+
+namespace greirat.Helpers
+{
+    /// <summary>
+    /// </summary>
+    public class OrderDataAsciiTableConverter
+    {
+        private const string ORDER_ID_COLUMN_NAME = "OrderID";
+        private const string PERSON_NAME_COLUMN_NAME = "PersonName";
+        private const string ORDER_TEXT_COLUMN_NAME = "OrderText";
+        private const string DISCORD_FORMATTER_SYMBOLS = "```";
+
+        private DataTable ShowOrderResultsDataTable { get; set; } = new();
+
+        public OrderDataAsciiTableConverter ()
+        {
+            CreatedOrderResultsDataTableColumns();
+        }
+
+        public StringBuilder FormOrdersShowData (Queue<OrderData> todayOrders)
+        {
+            FillTableWithData(todayOrders);
+
+            return GetAsciiTableBuilder();
+        }
+
+        private void CreatedOrderResultsDataTableColumns ()
+        {
+            ShowOrderResultsDataTable.Columns.Add(new DataColumn(ORDER_ID_COLUMN_NAME, typeof(int)));
+            ShowOrderResultsDataTable.Columns.Add(new DataColumn(PERSON_NAME_COLUMN_NAME, typeof(string)));
+            ShowOrderResultsDataTable.Columns.Add(new DataColumn(ORDER_TEXT_COLUMN_NAME, typeof(string)));
+        }
+
+        private void FillTableWithData (Queue<OrderData> todayOrders)
+        {
+            ShowOrderResultsDataTable.Clear();
+
+            while (todayOrders.Count > 0)
+            {
+                CreateTableRowFromOrderData(todayOrders.Dequeue());
+            }
+        }
+
+        private void CreateTableRowFromOrderData (OrderData order)
+        {
+            DataRow tempDataRow = ShowOrderResultsDataTable.NewRow();
+            tempDataRow[ORDER_ID_COLUMN_NAME] = order.OrderID;
+            tempDataRow[PERSON_NAME_COLUMN_NAME] = order.PersonName;
+            tempDataRow[ORDER_TEXT_COLUMN_NAME] = order.OrderText;
+            ShowOrderResultsDataTable.Rows.Add(tempDataRow);
+        }
+
+        private StringBuilder GetAsciiTableBuilder ()
+        {
+            StringBuilder asciiTableBuilder = AsciiTableGenerator.CreateAsciiTableFromDataTable(ShowOrderResultsDataTable);
+            PrepareBuilderToDiscordFormatting(asciiTableBuilder);
+
+            return asciiTableBuilder;
+        }
+
+        private static void PrepareBuilderToDiscordFormatting (StringBuilder asciiTableBuilder)
+        {
+            asciiTableBuilder.Insert(0, DISCORD_FORMATTER_SYMBOLS);
+            asciiTableBuilder.Append(DISCORD_FORMATTER_SYMBOLS);
+        }
+    }
+}

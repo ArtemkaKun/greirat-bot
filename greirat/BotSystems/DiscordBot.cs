43a891bd5d4d20737ef20eceeb032fb54e3ec5bc
Artem Yurchenko <zumikq78@gmail.com>
2021-06-06 13:43:45 +0200

    Bot code was separated from Program class

---
+++ greirat/BotSystems/DiscordBot.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace greirat
+{
+    public class DiscordBot
+    {
+        private const string ENVIRONMENT_VARIABLE_WITH_BOT_TOKEN = "FOOD_BOT_TOKEN";
+        private const char BOT_COMMAND_PREFIX = '!';
+
+    #if DEBUG
+        public bool BypassIsFromBotCheck { get; set; }
+        public DiscordSocketClient BotClient
+        {
+            get
+            {
+                return Client;
+            }
+        }
+
+    #endif
+        
+        private DiscordSocketClient Client { get; set; } = new();
+        private CommandService Commands { get; set; } = new();
+        private BotConfig Config { get; set; } = new();
+
+        #if DEBUG
+            public async Task SendMessageToChat ()
+            {
+                await Client.GetGuild(Config.CurrentConfigData.GuildIDForTests).GetTextChannel(Config.CurrentConfigData.ChannelIDForTests).SendMessageAsync("Test");
+            }
+        #endif
+        
+        public async Task Initialize ()
+        {
+            SubscribeOnClientEvents();
+            await GetCommandsModule();
+            await StartBot();
+        }
+        
+        private void SubscribeOnClientEvents ()
+        {
+            Client.Log += Log;
+            Client.MessageReceived += ProceedReceivedMessage;
+        }
+
+        private Task Log (LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        private async Task ProceedReceivedMessage (SocketMessage messageParam)
+        {
+            if (messageParam is not SocketUserMessage message)
+            {
+                return;
+            }
+
+            if (CheckIfMessageFromBot(message) == true)
+            {
+                return;
+            }
+
+            if (CheckIfTheMessageContainsCommand(message, out int commandPrefixPosition) == false)
+            {
+                return;
+            }
+
+            SocketCommandContext context = new(Client, message);
+            await Commands.ExecuteAsync(context, commandPrefixPosition, null);
+        }
+
+        private bool CheckIfMessageFromBot (SocketUserMessage message)
+        {
+        #if DEBUG
+            if (BypassIsFromBotCheck == true)
+            {
+                return false;
+            }
+        #endif
+
+            return message.Author.IsBot == true;
+        }
+
+        private bool CheckIfTheMessageContainsCommand (SocketUserMessage message, out int commandPrefixPosition)
+        {
+            commandPrefixPosition = 0;
+
+            return message.HasCharPrefix(BOT_COMMAND_PREFIX, ref commandPrefixPosition) == true;
+        }
+
+        private async Task GetCommandsModule ()
+        {
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+        }
+
+        private async Task StartBot ()
+        {
+            await Client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable(ENVIRONMENT_VARIABLE_WITH_BOT_TOKEN));
+            await Client.StartAsync();
+        }
+    }
+}
--- greirat/Program.cs
+++ greirat/Program.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Reflection;
-using System.Threading.Tasks;
-using Discord;
-using Discord.Commands;
-using Discord.WebSocket;
+﻿using System.Threading.Tasks;
 
 namespace greirat
 {
     internal class Program
     {
-        private const string ENVIRONMENT_VARIABLE_WITH_BOT_TOKEN = "FOOD_BOT_TOKEN";
-        private const char BOT_COMMAND_PREFIX = '!';
-
-        private DiscordSocketClient Client { get; set; } = new();
-        private CommandService Commands { get; set; } = new();
-
+        private DiscordBot Bot { get; set; } = new();
+        
         private static void Main ()
         {
             new Program().MainAsync().GetAwaiter().GetResult();
@@ -22,62 +13,8 @@ namespace greirat
 
         private async Task MainAsync ()
         {
-            SubscribeOnClientEvents();
-            await GetCommandsModule();
-            await StartBot();
+            await Bot.Initialize();
             await Task.Delay(-1);
-        }
-
-        private void SubscribeOnClientEvents ()
-        {
-            Client.Log += Log;
-            Client.MessageReceived += ProceedReceivedMessage;
-        }
-
-        private Task Log (LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-
-            return Task.CompletedTask;
-        }
-
-        private async Task ProceedReceivedMessage (SocketMessage messageParam)
-        {
-            if (messageParam is not SocketUserMessage message)
-            {
-                return;
-            }
-
-            if (message.Author.IsBot == true)
-            {
-                return;
-            }
-
-            if (CheckIfTheMessageContainsCommand(message, out int commandPrefixPosition) == false)
-            {
-                return;
-            }
-
-            SocketCommandContext context = new(Client, message);
-            await Commands.ExecuteAsync(context, commandPrefixPosition, null);
-        }
-
-        private bool CheckIfTheMessageContainsCommand (SocketUserMessage message, out int commandPrefixPosition)
-        {
-            commandPrefixPosition = 0;
-
-            return message.HasCharPrefix(BOT_COMMAND_PREFIX, ref commandPrefixPosition) == true;
-        }
-
-        private async Task GetCommandsModule ()
-        {
-            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
-        }
-
-        private async Task StartBot ()
-        {
-            await Client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable(ENVIRONMENT_VARIABLE_WITH_BOT_TOKEN));
-            await Client.StartAsync();
         }
     }
 }

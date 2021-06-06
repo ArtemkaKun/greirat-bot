1e16346c4db89167364ea89bd4ca4c9575c9a2f8
Artem Yurchenko <zumikq78@gmail.com>
2021-06-06 13:43:19 +0200

    BotConfig was created

---
+++ greirat/BotConfigSystems/BotConfigData.cs
@@ -0,0 +1,14 @@
+﻿namespace greirat
+{
+    public class BotConfigData
+    {
+        public ulong GuildIDForTests { get; set; }
+        public ulong ChannelIDForTests { get; set; }
+
+        public BotConfigData (ulong guildIdForTests, ulong channelIdForTests)
+        {
+            GuildIDForTests = guildIdForTests;
+            ChannelIDForTests = channelIdForTests;
+        }
+    }
+}

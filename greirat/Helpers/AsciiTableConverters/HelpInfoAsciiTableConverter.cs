960f43dffedfc7faf6368074df4fb274dc09444a
Artem Yurchenko <zumikq78@gmail.com>
2021-06-12 15:19:59 +0200

    Aliases were hook up in the help command

--- greirat/Helpers/AsciiTableConverters/HelpInfoAsciiTableConverter.cs
+++ greirat/Helpers/AsciiTableConverters/HelpInfoAsciiTableConverter.cs
@@ -11,12 +11,14 @@ namespace greirat.Helpers
     {
         private const string COMMAND_COLUMN_NAME = "Command";
         private const string COMMAND_DESCRIPTION_COLUMN_NAME = "Description";
-        private const string COMMAND_NAME_TEMPLATE = "!{0}{1}";
+        private const string COMMAND_NAME_TEMPLATE = "!{0}";
         private const string COMMAND_ARGUMENT_NAME_TEMPLATE = " <{0}>";
+        private const string TEXT_BETWEEN_COMMAND_NAMES = ", ";
 
         public string HelpInfoInTableForm { get; private set; }
-        
+
         private DataTable ShowHelpResultsDataTable { get; set; } = new();
+        private StringBuilder CommandNamesBuilder { get; set; } = new();
 
         public HelpInfoAsciiTableConverter ()
         {
@@ -48,9 +50,33 @@ namespace greirat.Helpers
         private void CreateTableRowFromOrderData (MethodInfo order)
         {
             DataRow tempDataRow = ShowHelpResultsDataTable.NewRow();
-            tempDataRow[COMMAND_COLUMN_NAME] = string.Format(COMMAND_NAME_TEMPLATE, order.GetCustomAttribute<CommandAttribute>()?.Text, GetMethodArgumentNames(order));
+            tempDataRow[COMMAND_COLUMN_NAME] = $"{GetCommandNameWithAliases(order)}{GetMethodArgumentNames(order)}";
             tempDataRow[COMMAND_DESCRIPTION_COLUMN_NAME] = order.GetCustomAttribute<SummaryAttribute>()?.Text ?? string.Empty;
             ShowHelpResultsDataTable.Rows.Add(tempDataRow);
+        }
+
+        private string GetCommandNameWithAliases (MethodInfo order)
+        {
+            CommandNamesBuilder.Clear();
+            AppendCommandName(order.GetCustomAttribute<CommandAttribute>()?.Text);
+            string[] commandAliases = order.GetCustomAttribute<AliasAttribute>()?.Aliases;
+
+            for (int aliasPointer = 0; aliasPointer < commandAliases?.Length; aliasPointer++)
+            {
+                AppendCommandName(commandAliases[aliasPointer]);
+            }
+
+            return CommandNamesBuilder.ToString();
+        }
+
+        private void AppendCommandName (string commandName)
+        {
+            if (CommandNamesBuilder.Length > 0)
+            {
+                CommandNamesBuilder.Append(TEXT_BETWEEN_COMMAND_NAMES);
+            }
+
+            CommandNamesBuilder.AppendFormat(COMMAND_NAME_TEMPLATE, commandName);
         }
 
         private string GetMethodArgumentNames (MethodInfo order)
@@ -61,7 +87,7 @@ namespace greirat.Helpers
             {
                 argumentsNamesStringBuilder.Append(string.Format(COMMAND_ARGUMENT_NAME_TEMPLATE, argument.Name));
             }
-            
+
             return argumentsNamesStringBuilder.ToString();
         }
     }

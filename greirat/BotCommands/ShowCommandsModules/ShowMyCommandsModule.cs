b7fdadd058a845f7c78debca1d4db8a5ee52458f
Artem Yurchenko <zumikq78@gmail.com>
2021-06-17 20:35:07 +0200

    Sort and sum commands were implemented for the shmy command

--- greirat/BotCommands/ShowCommandsModules/ShowMyCommandsModule.cs
+++ greirat/BotCommands/ShowCommandsModules/ShowMyCommandsModule.cs
@@ -13,6 +13,20 @@ namespace greirat
         {
             return ShowOrdersData(COMMON_SHOW_COMMAND_NAME, GetAllUserTodayOrders());
         }
+        
+        [Command(SORT_SHOW_COMMAND_NAME)]
+        [Summary("Shows your today's orders (sorted a -> z)")]
+        public Task ShowUserTodayOrdersSorted ()
+        {
+            return ShowOrdersData(SORT_SHOW_COMMAND_NAME, GetAllUserTodayOrders());
+        }
+        
+        [Command(SUM_SHOW_COMMAND_NAME)]
+        [Summary("Shows your today's orders (summary mode)")]
+        public Task ShowUserTodayOrdersSummary ()
+        {
+            return ShowOrdersData(SUM_SHOW_COMMAND_NAME, GetAllUserTodayOrders());
+        }
 
         private Queue<OrderData> GetAllUserTodayOrders ()
         {

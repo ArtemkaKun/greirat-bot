4f33ce32e1b1f2dd2a2001889152e3edcf1d6048
Artem Yurchenko <zumikq78@gmail.com>
2021-06-17 19:54:13 +0200

    Similar constants for different commands modules were moved into CommandsDatabase class

---
+++ greirat/BotCommands/CommandsDatabase.cs
@@ -0,0 +1,9 @@
+﻿namespace greirat
+{
+    public static class CommandsDatabase
+    {
+        public const string NOTHING_TO_SHOW_MESSAGE = "Nothing to show";
+        public const string REMINDER_WAS_SET_MESSAGE = "Reminder was set on {0} everyday (except weekends)";
+        public const string COMMAND_NOT_IMPLEMENTED_MESSAGE = "This command isn't implemented yet";
+    }
+}
--- greirat/BotCommands/CommandsModule.cs
+++ greirat/BotCommands/CommandsModule.cs
@@ -9,15 +9,13 @@ namespace greirat
 {
     public class CommandsModule : ModuleBase<SocketCommandContext>
     {
-        private const string ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE = "Order was successfully ";
         private const string ORDER_WAS_SAVED_MESSAGE = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "proceeded";
         private const string ORDER_WAS_UPDATED_MESSAGE = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "updated";
         private const string ORDER_WAS_REMOVED = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "removed";
         private const string ORDER_UPDATE_FAILED = "Failed to update order";
         private const string ORDER_DELETE_FAILED = "Failed to remove order";
-        private const string NOTHING_TO_SHOW_MESSAGE = "Nothing to show";
-        private const string REMINDER_WAS_SET_MESSAGE = "Reminder was set on {0} everyday (except weekends)";
-
+        private const string ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE = "Order was successfully ";
+        
         private OrderDataAsciiTableConverter OrdersOutputMaintainer { get; set; } = new();
         private HelpInfoAsciiTableConverter HelpOutputMaintainer { get; set; } = new();
         
@@ -46,7 +44,7 @@ namespace greirat
         {
             Queue<OrderData> todayOrders = Program.DBManager.GetTodayOrders(Context.Message.Author.Username);
 
-            return ReplyAsync(todayOrders.Count == 0 ? NOTHING_TO_SHOW_MESSAGE : OrdersOutputMaintainer.FormOrdersShowData(todayOrders).ToString());
+            return ReplyAsync(todayOrders.Count == 0 ? CommandsDatabase.NOTHING_TO_SHOW_MESSAGE : OrdersOutputMaintainer.FormOrdersShowData(todayOrders).ToString());
         }
 
         [Command("updateOrder")]
@@ -77,14 +75,13 @@ namespace greirat
             FoodRemindData newReminderID = Program.DBManager.AddNewReminder(Context, timeOfDayWhereRemind, messageToRemind);
             new OrdersReminder(newReminderID).TryStartReminderThread();
             
-            return ReplyAsync(string.Format(REMINDER_WAS_SET_MESSAGE, timeOfDayWhereRemind));
+            return ReplyAsync(string.Format(CommandsDatabase.REMINDER_WAS_SET_MESSAGE, timeOfDayWhereRemind));
         }
     }
     
     [Group("shall")]
     public class ShowCommandModule : ModuleBase<SocketCommandContext>
     {
-        private const string NOTHING_TO_SHOW_MESSAGE = "Nothing to show";
         private const string COMMON_SHOW_COMMAND_NAME = "";
         private const string SORT_SHOW_COMMAND_NAME = "-sort";
         private const string SUM_SHOW_COMMAND_NAME = "-sum";
@@ -127,7 +124,7 @@ namespace greirat
         {
             Queue<OrderData> todayOrders = Program.DBManager.GetTodayOrders();
 
-            return ReplyAsync(todayOrders.Count == 0 ? NOTHING_TO_SHOW_MESSAGE : ShowAllOptionsFunctions[command]?.Invoke(todayOrders).ToString());
+            return ReplyAsync(todayOrders.Count == 0 ? CommandsDatabase.NOTHING_TO_SHOW_MESSAGE : ShowAllOptionsFunctions[command]?.Invoke(todayOrders).ToString());
         }
     }
 }

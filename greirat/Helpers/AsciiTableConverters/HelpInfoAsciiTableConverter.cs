d7385a27469310137071d0e37fcc6c2dc30bf926
Artem Yurchenko <zumikq78@gmail.com>
2021-06-19 11:30:51 +0200

    Commands from all commandModulesClasses were hooked up in the help info; HelpInfoAciiTableConverter class naming was fixed

--- greirat/Helpers/AsciiTableConverters/HelpInfoAsciiTableConverter.cs
+++ greirat/Helpers/AsciiTableConverters/HelpInfoAsciiTableConverter.cs
@@ -22,51 +22,72 @@ namespace greirat.Helpers
 
         public HelpInfoAsciiTableConverter ()
         {
-            CreatedOrderResultsDataTableColumns();
-            HelpInfoInTableForm = FormOrdersShowData(typeof(OrderCommandsModule).GetMethods().Where(method => method.GetCustomAttribute<CommandAttribute>() != null).ToArray()).ToString();
+            CreatedCommandsDataTableColumns();
+            MethodInfo[] commandsMethods = GetAllCommandsMethods();
+            HelpInfoInTableForm = FormCommandsShowData(commandsMethods).ToString();
         }
 
-        private void CreatedOrderResultsDataTableColumns ()
+        private void CreatedCommandsDataTableColumns ()
         {
             ShowHelpResultsDataTable.Columns.Add(new DataColumn(COMMAND_COLUMN_NAME, typeof(string)));
             ShowHelpResultsDataTable.Columns.Add(new DataColumn(COMMAND_DESCRIPTION_COLUMN_NAME, typeof(string)));
         }
 
-        private StringBuilder FormOrdersShowData (MethodInfo[] todayOrders)
+        private MethodInfo[] GetAllCommandsMethods ()
         {
-            FillTableWithData(todayOrders);
+            return Assembly.GetEntryAssembly()?.GetTypes().Where(typeUnit => typeUnit.IsAssignableTo(typeof(ModuleBase<SocketCommandContext>)))
+                           .SelectMany(commandsModule => commandsModule.GetMethods().Where(method => method.GetCustomAttribute<CommandAttribute>() != null))
+                           .ToArray();
+        }
+
+        private StringBuilder FormCommandsShowData (MethodInfo[] commandMethods)
+        {
+            FillTableWithData(commandMethods);
 
             return GetAsciiTableBuilder(ShowHelpResultsDataTable);
         }
 
-        private void FillTableWithData (MethodInfo[] todayOrders)
+        private void FillTableWithData (MethodInfo[] commandMethods)
         {
-            for (int methodPointer = 0; methodPointer < todayOrders.Length; methodPointer++)
+            for (int methodPointer = 0; methodPointer < commandMethods.Length; methodPointer++)
             {
-                CreateTableRowFromOrderData(todayOrders[methodPointer]);
+                CreateTableRowFromOrderData(commandMethods[methodPointer]);
             }
         }
 
-        private void CreateTableRowFromOrderData (MethodInfo order)
+        private void CreateTableRowFromOrderData (MethodInfo command)
         {
             DataRow tempDataRow = ShowHelpResultsDataTable.NewRow();
-            tempDataRow[COMMAND_COLUMN_NAME] = $"{GetCommandNameWithAliases(order)}{GetMethodArgumentNames(order)}";
-            tempDataRow[COMMAND_DESCRIPTION_COLUMN_NAME] = order.GetCustomAttribute<SummaryAttribute>()?.Text ?? string.Empty;
+            tempDataRow[COMMAND_COLUMN_NAME] = $"{GetCommandNameWithAliases(command)}{GetMethodArgumentNames(command)}";
+            tempDataRow[COMMAND_DESCRIPTION_COLUMN_NAME] = command.GetCustomAttribute<SummaryAttribute>()?.Text ?? string.Empty;
             ShowHelpResultsDataTable.Rows.Add(tempDataRow);
         }
 
-        private string GetCommandNameWithAliases (MethodInfo order)
+        private string GetCommandNameWithAliases (MethodInfo command)
         {
             CommandNamesBuilder.Clear();
-            AppendCommandName(order.GetCustomAttribute<CommandAttribute>()?.Text);
-            string[] commandAliases = order.GetCustomAttribute<AliasAttribute>()?.Aliases;
+            AppendCommandName(GetCommandName(command));
+            AppendAllAliases(command);
+
+            return CommandNamesBuilder.ToString();
+        }
+
+        private string GetCommandName (MethodInfo command)
+        {
+            string commandsGroupName = command.DeclaringType?.GetCustomAttribute<GroupAttribute>()?.Prefix;
+            string commandMethodName = command.GetCustomAttribute<CommandAttribute>()?.Text;
+            
+            return string.IsNullOrEmpty(commandsGroupName) == false ? $"{commandsGroupName} {commandMethodName}" : commandMethodName;
+        }
+
+        private void AppendAllAliases (MethodInfo command)
+        {
+            string[] commandAliases = command.GetCustomAttribute<AliasAttribute>()?.Aliases;
 
             for (int aliasPointer = 0; aliasPointer < commandAliases?.Length; aliasPointer++)
             {
                 AppendCommandName(commandAliases[aliasPointer]);
             }
-
-            return CommandNamesBuilder.ToString();
         }
 
         private void AppendCommandName (string commandName)
@@ -79,11 +100,11 @@ namespace greirat.Helpers
             CommandNamesBuilder.AppendFormat(COMMAND_NAME_TEMPLATE, commandName);
         }
 
-        private string GetMethodArgumentNames (MethodInfo order)
+        private string GetMethodArgumentNames (MethodInfo command)
         {
             StringBuilder argumentsNamesStringBuilder = new();
 
-            foreach (ParameterInfo argument in order.GetParameters())
+            foreach (ParameterInfo argument in command.GetParameters())
             {
                 argumentsNamesStringBuilder.Append(string.Format(COMMAND_ARGUMENT_NAME_TEMPLATE, argument.Name));
             }

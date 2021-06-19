ef66ccad0cbbb0abcf73ff68cd0575b96937518d
Artem Yurchenko <zumikq78@gmail.com>
2021-06-19 12:37:30 +0200

    Cannot add reminder message become a const in ReminderCommandsModule class; reminder const from CommandsDatabase class was moved to the ReminderCommandsModule class

--- greirat/BotCommands/CommandsDatabase.cs
+++ greirat/BotCommands/CommandsDatabase.cs
@@ -3,7 +3,6 @@
     public static class CommandsDatabase
     {
         public const string NOTHING_TO_SHOW_MESSAGE = "Nothing to show";
-        public const string REMINDER_WAS_SET_MESSAGE = "Reminder was set on {0} everyday (except weekends)";
         public const string COMMAND_NOT_IMPLEMENTED_MESSAGE = "This command isn't implemented yet";
     }
 }
--- greirat/BotCommands/ReminderCommandsModule.cs
+++ greirat/BotCommands/ReminderCommandsModule.cs
@@ -6,13 +6,16 @@ namespace greirat
     [Group("remind")]
     public class ReminderCommandsModule : ModuleBase<SocketCommandContext>
     {
+        private const string REMINDER_WAS_SET_MESSAGE = "Reminder was set on {0} everyday (except weekends)";
+        private const string CANNOT_SET_REMINDER_MESSAGE = "Cannot set reminder for this channel";
+        
         [Command("-set")]
         [Summary("Sets reminder about of food orders")]
         public Task SetEverydayReminder (string timeOfDayWhereRemind, [Remainder] string messageToRemind)
         {
             bool isOperationSucceed = Program.RemindersOrchestrator.TryStartNewReminder(Context, timeOfDayWhereRemind, messageToRemind);
 
-            return ReplyAsync(isOperationSucceed == true ? string.Format(CommandsDatabase.REMINDER_WAS_SET_MESSAGE, timeOfDayWhereRemind) : "Cannot set reminder for this channel");
+            return ReplyAsync(isOperationSucceed == true ? string.Format(REMINDER_WAS_SET_MESSAGE, timeOfDayWhereRemind) : CANNOT_SET_REMINDER_MESSAGE);
         }
     }
 }

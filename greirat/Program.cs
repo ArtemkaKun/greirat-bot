0be95c0920b0d7ae547a9b6547c45261623366f0
Artem Yurchenko <zumikq78@gmail.com>
2021-06-19 12:01:22 +0200

    All code in Program class connected to Reminders was moved into separate class

--- greirat/Program.cs
+++ greirat/Program.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace greirat
 {
     internal class Program
     {
-        private const string REMINDERS_WERE_ACTIVATED_MESSAGE = "All reminders from DB were activated";
-        
         public static DiscordBot Bot { get; private set; } = new();
         public static DB DBManager { get; private set; } = new();
-        public static List<OrdersReminder> ActiveReminders { get; private set; }
+        public static RemindersManager RemindersOrchestrator { get; private set; }
         
         private static void Main ()
         {
@@ -21,36 +17,8 @@ namespace greirat
         {
             await Bot.Initialize();
             DBManager.EnsureThatDBIsCreated();
-            await StartReminders();
+            await RemindersOrchestrator.StartRemindersFromDB();
             await Task.Delay(-1);
-        }
-
-        private static async Task StartReminders ()
-        {
-            CollectRemindersFromDB();
-            StartAllReminders();
-            Console.WriteLine(REMINDERS_WERE_ACTIVATED_MESSAGE);
-
-            await Task.Yield();
-        }
-
-        private static void CollectRemindersFromDB ()
-        {
-            Stack<FoodRemindData> remindersCollection = DBManager.GetAllRemindersFromDB();
-            ActiveReminders = new List<OrdersReminder>(remindersCollection.Count);
-            
-            while (remindersCollection.Count > 0)
-            {
-                ActiveReminders.Add(new OrdersReminder(remindersCollection.Pop()));
-            }
-        }
-
-        private static void StartAllReminders ()
-        {
-            for (int reminderPointer = 0; reminderPointer < ActiveReminders.Count; reminderPointer++)
-            {
-                ActiveReminders[reminderPointer].TryStartReminderThread();
-            }
         }
     }
 }   
---
+++ greirat/RemindersManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace greirat
+{
+    public class RemindersManager
+    {
+        private const string REMINDERS_WERE_ACTIVATED_MESSAGE = "All reminders from DB were activated";
+        
+        public static List<OrdersReminder> ActiveReminders { get; private set; }
+        
+        public async Task StartRemindersFromDB ()
+        {
+            CollectRemindersFromDB();
+            StartAllReminders();
+            Console.WriteLine(REMINDERS_WERE_ACTIVATED_MESSAGE);
+
+            await Task.Yield();
+        }
+
+        private static void CollectRemindersFromDB ()
+        {
+            Stack<FoodRemindData> remindersCollection = Program.DBManager.GetAllRemindersFromDB();
+            ActiveReminders = new List<OrdersReminder>(remindersCollection.Count);
+            
+            while (remindersCollection.Count > 0)
+            {
+                ActiveReminders.Add(new OrdersReminder(remindersCollection.Pop()));
+            }
+        }
+
+        private static void StartAllReminders ()
+        {
+            for (int reminderPointer = 0; reminderPointer < ActiveReminders.Count; reminderPointer++)
+            {
+                ActiveReminders[reminderPointer].TryStartReminderThread();
+            }
+        }
+    }
+}

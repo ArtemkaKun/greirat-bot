3be625144bd4c56b7077853a8138cb07a1d554e1
Artem Yurchenko <zumikq78@gmail.com>
2021-06-20 14:38:33 +0200

    Cancelling of active reminder thread was implemented; edit reminder command was fixed

--- greirat/BotSystems/VoteReminder.cs
+++ greirat/BotSystems/VoteReminder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace greirat
@@ -6,57 +7,90 @@ namespace greirat
     public class VoteReminder
     {
         public VoteRemindData ReminderData { get; private set; }
-        
+
         private TimeSpan RemindTime { get; set; }
+        private Task ReminderTimerTask { get; set; }
+        private CancellationTokenSource ReminderCancellationProvider { get; set; }
+        private CancellationToken ReminderCancellationToken { get; set; }
 
         public VoteReminder (VoteRemindData reminderData)
         {
             ReminderData = reminderData;
+            SetUpCancellationMembers();
         }
 
         public void TryStartReminderThread ()
         {
+            if (ReminderTimerTask != null)
+            {
+                CancelOldReminderThread();
+                SetUpCancellationMembers();
+            }
+
             if (CheckIfCanStartReminderThread() == true)
             {
-                RemindAboutVote();
+                ReminderTimerTask = new Task(RemindAboutVote);
+                ReminderTimerTask.Start();
             }
+        }
+
+        private void CancelOldReminderThread ()
+        {
+            ReminderCancellationProvider.Cancel();
+            ReminderTimerTask.Dispose();
+        }
+
+        private void SetUpCancellationMembers ()
+        {
+            ReminderCancellationProvider = new CancellationTokenSource();
+            ReminderCancellationToken = ReminderCancellationProvider.Token;
         }
 
         private bool CheckIfCanStartReminderThread ()
         {
             string timeForRemind = ReminderData.TimeToRemind;
+            
             return (string.IsNullOrEmpty(timeForRemind) == false) && (TimeSpan.TryParse(timeForRemind, out _) == true);
         }
 
-        private async Task RemindAboutVote ()
+        private async void RemindAboutVote ()
         {
             RemindTime = TimeSpan.Parse(ReminderData.TimeToRemind);
-            
+
             while (true)
             {
                 TimeSpan timeToWait = CalculateTimeToRemind();
-                await Task.Delay(timeToWait);
+
+                try
+                {
+                    await Task.Delay(timeToWait, ReminderCancellationToken);
+                }
+                catch (Exception)
+                {
+                    return;
+                }
+
                 await Program.Bot.SendMessage(ReminderData.GuildID, ReminderData.ChannelID, ReminderData.RemindMessage);
             }
         }
 
         private TimeSpan CalculateTimeToRemind ()
         {
-            if (CheckIfTodayIsWeekend() == true)
-            {
-                return new TimeSpan(24, 0, 0);
-            }
-            
+            // if (CheckIfTodayIsWeekend() == true)
+            // {
+            //     return new TimeSpan(24, 0, 0);
+            // }
+
             TimeSpan currentTime = DateTime.Now.TimeOfDay;
             TimeSpan timeToWait = currentTime < RemindTime ? RemindTime.Subtract(currentTime) : DateTime.Today.Subtract(currentTime).TimeOfDay + RemindTime;
-            
+
             return timeToWait;
         }
 
         private bool CheckIfTodayIsWeekend ()
         {
             DayOfWeek todayDay = DateTime.Today.DayOfWeek;
-            
+
             return todayDay is DayOfWeek.Saturday or DayOfWeek.Sunday;
         }
     }

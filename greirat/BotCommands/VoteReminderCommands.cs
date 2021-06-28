529017d60276416de0f2c09ef34faf108a48f87b
Artem Yurchenko <zumikq78@gmail.com>
2021-06-28 19:55:13 +0200

    VoteReminder commands descriptions were moved to constants

--- greirat/BotCommands/VoteReminderCommands.cs
+++ greirat/BotCommands/VoteReminderCommands.cs
@@ -8,14 +8,20 @@ namespace greirat
     public class VoteReminderCommands : ModuleBase<SocketCommandContext>
     {
         private const string VOTE_REMINDER_COMMANDS_GROUP_NAME = "voteReminder";
+        
         private const string REMINDER_WAS_SET_MESSAGE = "Reminder was set on {0} everyday (except weekends)";
         private const string CANNOT_SET_REMINDER_MESSAGE = "Cannot set reminder for this channel";
         private const string NO_REMINDER_IN_CHANNEL_MESSAGE = "No reminders in channet yet";
         private const string REMINDER_WAS_REMOVED_MESSAGE = "Reminder was successfully removed";
         private const string REMINDER_WAS_UPDATED_MESSAGE = "Reminder was successfully updated";
+        
+        private const string SET_REMINDER_COMMAND_DESCRIPTION = "Sets food vote reminder";
+        private const string SHOW_REMINDER_COMMAND_DESCRIPTION = "Show channel's reminder data";
+        private const string DELETE_REMINDER_COMMAND_DESCRIPTION = "Delete channel's reminder";
+        private const string UPDATE_REMINDER_COMMAND_DESCRIPTION = "Update channel's reminder";
 
         [Command(CommandsDatabase.SET_COMMAND_NAME)]
-        [Summary("Sets food vote reminder")]
+        [Summary(SET_REMINDER_COMMAND_DESCRIPTION)]
         public Task SetEverydayVoteReminder (string remindTime, [Optional] int voteDurationInMinutes, [Remainder] string remindMessage)
         {
             bool isOperationSucceed = Program.VoteRemindersController.TryStartNewVoteReminder(Context, remindTime, remindMessage, voteDurationInMinutes);
@@ -24,7 +30,7 @@ namespace greirat
         }
 
         [Command(CommandsDatabase.SHOW_COMMAND_NAME)]
-        [Summary("Show channel's reminder data")]
+        [Summary(SHOW_REMINDER_COMMAND_DESCRIPTION)]
         public Task ShowChannelVoteReminderData ()
         {
             string infoAboutReminder = Program.VoteRemindersController.GetVoteReminderInfo(Context.Guild.Id, Context.Message.Channel.Id);
@@ -33,7 +39,7 @@ namespace greirat
         }
 
         [Command(CommandsDatabase.DELETE_COMMAND_NAME)]
-        [Summary("Delete channel's reminder")]
+        [Summary(DELETE_REMINDER_COMMAND_DESCRIPTION)]
         public Task DeleteChannelVoteReminder ()
         {
             bool isOperationSucceed = Program.VoteRemindersController.TryDeleteChannelVoteReminder(Context.Guild.Id, Context.Message.Channel.Id);
@@ -42,7 +48,7 @@ namespace greirat
         }
 
         [Command(CommandsDatabase.UPDATE_COMMAND_NAME)]
-        [Summary("Update channel's reminder")]
+        [Summary(UPDATE_REMINDER_COMMAND_DESCRIPTION)]
         public Task UpdateChannelVoteReminder (string remindTime, [Remainder] string remindMessage)
         {
             bool isOperationSucceed = Program.VoteRemindersController.TryUpdateChannelVoteReminder(Context, remindTime, remindMessage);

c224f1fce927da6206462390ad21a82522bd9805
Artem Yurchenko <zumikq78@gmail.com>
2021-07-01 20:13:58 +0200

    SimpleOrderInfo class was implemented

---
+++ greirat/BotCommands/OrderCommandsModule/SimpleOrderInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace BotCommands
+{
+    public class SimpleOrderInfo
+    {
+        public int OrderID { get; }
+        public string OrderMessage { get; }
+        public string OrderOwner { get; private set; }
+
+        public SimpleOrderInfo (string orderMessage)
+        {
+            OrderMessage = orderMessage;
+        }
+
+        public SimpleOrderInfo (int orderId)
+        {
+            OrderID = orderId;
+        }
+
+        public SimpleOrderInfo (int orderId, string orderMessage)
+        {
+            OrderID = orderId;
+            OrderMessage = orderMessage;
+        }
+
+        public void SetOrderOwner (string orderOwner)
+        {
+            OrderOwner = orderOwner;
+        }
+    }
+}

d52e070fea8a7e6a43ae95ad2e000ed75d8d9adc
Artem Yurchenko <zumikq78@gmail.com>
2021-07-01 19:50:35 +0200

    A few string consts were moved to the separate file

--- greirat/BotCommands/OrderCommandsModule/OrderCommands.cs
+++ greirat/BotCommands/OrderCommandsModule/OrderCommands.cs
@@ -4,13 +4,9 @@ using greirat;
 
 namespace BotCommands
 {
-    [Group(ORDER_COMMANDS_GROUP_NAME)]
+    [Group(OrderCommandsModuleDatabase.ORDER_COMMANDS_GROUP_NAME)]
     public class OrderCommands : ModuleBase<SocketCommandContext>
     {
-        private const string ORDER_COMMANDS_GROUP_NAME = "order";
-        private const string CREATE_NEW_ORDER_COMMAND_DESCRIPTION = "Creates a new order";
-        private const string UPDATE_USER_ORDER_COMMAND_DESCRIPTION = "Updates order with provided text";
-        private const string DELETE_ORDER_COMMAND_DESCRIPTION = "Deletes order";
         private const string ORDER_WAS_SAVED_MESSAGE = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "proceeded";
         private const string ORDER_WAS_UPDATED_MESSAGE = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "updated";
         private const string ORDER_WAS_REMOVED = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "removed";
@@ -19,7 +15,7 @@ namespace BotCommands
         private const string ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE = "Order was successfully ";
 
         [Command(CommandsDatabase.MAKE_COMMAND_NAME)]
-        [Summary(CREATE_NEW_ORDER_COMMAND_DESCRIPTION)]
+        [Summary(OrderCommandsModuleDatabase.CREATE_NEW_ORDER_COMMAND_DESCRIPTION)]
         public Task CreateNewOrder ([Remainder] string orderText)
         {
             Program.DBManager.AddNewOrder(Context.Message.Author.Username, orderText);
@@ -28,7 +24,7 @@ namespace BotCommands
         }
 
         [Command(CommandsDatabase.UPDATE_COMMAND_NAME)]
-        [Summary(UPDATE_USER_ORDER_COMMAND_DESCRIPTION)]
+        [Summary(OrderCommandsModuleDatabase.UPDATE_USER_ORDER_COMMAND_DESCRIPTION)]
         public Task UpdateUserOrder (int idOfOrder, [Remainder] string newOrderText)
         {
             bool updateOperationResult = Program.DBManager.TryUpdateOrderData(Context.Message.Author.Username, idOfOrder, newOrderText);
@@ -37,7 +33,7 @@ namespace BotCommands
         }
 
         [Command(CommandsDatabase.DELETE_COMMAND_NAME)]
-        [Summary(DELETE_ORDER_COMMAND_DESCRIPTION)]
+        [Summary(OrderCommandsModuleDatabase.DELETE_ORDER_COMMAND_DESCRIPTION)]
         public Task DeleteOrder (int idOfOrder)
         {
             bool deleteOperationResult = Program.DBManager.TryDeleteOrderData(Context.Message.Author.Username, idOfOrder);
---
+++ greirat/BotCommands/OrderCommandsModule/OrderCommandsModuleDatabase.cs
@@ -0,0 +1,10 @@
+﻿namespace BotCommands
+{
+    public static class OrderCommandsModuleDatabase
+    {
+        public const string ORDER_COMMANDS_GROUP_NAME = "order";
+        public const string CREATE_NEW_ORDER_COMMAND_DESCRIPTION = "Creates a new order";
+        public const string UPDATE_USER_ORDER_COMMAND_DESCRIPTION = "Updates order with provided text";
+        public const string DELETE_ORDER_COMMAND_DESCRIPTION = "Deletes order";
+    }
+}

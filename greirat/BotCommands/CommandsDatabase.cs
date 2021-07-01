a3db22fafaedcb342f6276c3f331f6577d050b4a
Artem Yurchenko <zumikq78@gmail.com>
2021-07-01 20:16:04 +0200

    -show command was changed on -sh

--- greirat/BotCommands/CommandsDatabase.cs
+++ greirat/BotCommands/CommandsDatabase.cs
@@ -5,7 +5,7 @@
         public const string NOTHING_TO_SHOW_MESSAGE = "Nothing to show";
         public const string MAKE_COMMAND_NAME = "-mk";
         public const string SET_COMMAND_NAME = "-set";
-        public const string SHOW_COMMAND_NAME = "-show";
+        public const string SHOW_COMMAND_NAME = "-sh";
         public const string DELETE_COMMAND_NAME = "-del";
         public const string UPDATE_COMMAND_NAME = "-upd";
     }

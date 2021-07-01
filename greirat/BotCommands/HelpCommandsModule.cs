61934dafefbf7f0f3a7e784e4b46a5a64c57c6a0
Artem Yurchenko <zumikq78@gmail.com>
2021-07-01 20:28:51 +0200

    string values become consts in HelpCommandsModule class

--- greirat/BotCommands/HelpCommandsModule.cs
+++ greirat/BotCommands/HelpCommandsModule.cs
@@ -5,9 +5,13 @@ namespace greirat
 {
     public class HelpCommandsModule : ModuleBase<SocketCommandContext>
     {
-        [Command("help")]
-        [Alias("h")]
-        [Summary("Shows bot's commands")]
+        private const string HELP_COMMAND_NAME = "help";
+        private const string HELP_COMMAND_ALIAS_NAME = "h";
+        private const string HELP_COMMAND_DESCRIPTION = "Shows bot's commands";
+        
+        [Command(HELP_COMMAND_NAME)]
+        [Alias(HELP_COMMAND_ALIAS_NAME)]
+        [Summary(HELP_COMMAND_DESCRIPTION)]
         public Task ShowHelpMessage ()
         {
             return ReplyAsync(Program.HelpOutputMaintainer.HelpInfoInTableForm);
